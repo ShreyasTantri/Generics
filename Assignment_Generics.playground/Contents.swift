2ecb62cbba91a61a6354252f617234a650c72981
ShreyasTantri <shreyastantrik29@gmail.com>
2026-01-11 12:02:56 +0530

    Generics Assignment

---
+++ Assignment_Generics.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+
+/*
+ Extend an Array to add a new method called allEqual only if elements are Equatable. It will return a boolean if all are equal, we can use allSatisfy()
+
+Example:
+[1,2,3,4,5].allEqual() that will return a boolean out of this
+
+*/
+
+let elementsNE: [Int] = [3, 6, 12, 33, 55]
+let elementsE: [Int] = [3, 3, 3, 3, 3]
+
+
+extension Array {
+    func allEqual() {
+        
+    }
+}
